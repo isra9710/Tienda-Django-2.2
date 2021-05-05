3ea04ecc25e0b35d40f8c229cfda4f4df514d932
isra9710 <isra.rios.con@gmail.com>
2021-05-05 14:16:12 -0500

    1.5.2

--- store/carts/views.py
+++ store/carts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart(request):
-    request.session['cart_id'] = '123'#Crear una sesión
-    valor = request.session.get('cart_id') #Obtener el valor de una sesión
-    print(valor)
-    request.session['cart_id']= None #Eliminar una sesión
+    user = request.user if request.user.is_authenticated else None
+    if request.session.get('cart_id'):
+        cart = Cart.objects.get(pk=request.session.get('cart_id'))#Obtenemos el carrito de base de datos
+    else:
+        cart = Cart.objects.create(user=user)
+    request.session['cart_id'] = cart.id
     return render(request, 'carts/cart.html')
