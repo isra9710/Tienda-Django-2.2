86a67986b3d1760811fa52852ad033b1415fbd71
isra9710 <isra.rios.con@gmail.com>
2021-05-06 17:44:30 -0500

    1.7.2

--- store/shipping_addresses/urls.py
+++ store/shipping_addresses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'shipping_addresses'
 
-url_patterns = [
+urlpatterns = [
     path('', views.ShippingAddressListView.as_view(), name='shipping_addresses'),
+    path('nueva', views.create, name='create'),
 ]
--- store/shipping_addresses/views.py
+++ store/shipping_addresses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import ShippingAddress
+from .forms import ShippingAddressForm
 # Create your views here.
 class ShippingAddressListView(ListView):
     model = ShippingAddress
@@ -9,3 +10,12 @@ class ShippingAddressListView(ListView):
     
     def get_queryset(self):
         return ShippingAddress.objects.filter(user = self.request.user).order_by('-default')
+    
+    
+def create(request):
+    form = ShippingAddressForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        shipping_address = form.save()
+    return render(request, 'shipping_addresses/create.html',{
+     'form':form   
+    })
